007b4921ec08c0204ddcb140282d21d51664337f
soporificpanda <51345809+soporificpanda@users.noreply.github.com>
2023-02-18 23:27:18 -0500

    Create cf-ddns.sh

---
+++ cf-ddns.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ZONE_ID='zone id'
+TOKEN='api token'
+
+RECORD_ID='dns record id'
+RECORD_NAME='example.com'
+
+#IPV4
+#PUBLIC_IP=$(curl -s 'ipv4.icanhazip.com')
+#CURRENT_RECORD=$(dig @1.1.1.1 +short $RECORD_NAME)
+
+#IPV6
+PUBLIC_IP=$(curl -s icanhazip.com)
+CURRENT_RECORD=$(dig @1.1.1.1 AAAA +short $RECORD_NAME)
+
+
+#change AAAA to A for ipv4
+data_json="{\"type\":\"AAAA\",\"name\":\"$RECORD_NAME\",\"content\":\"$PUBLIC_IP\"}"
+
+
+if [[ $CURRENT_RECORD = $PUBLIC_IP ]]
+then
+        echo "IP was already up to date" | tee ~/cf-ddns/cf-ddns.log
+        echo "$CURRENT_RECORD = DNS" | tee -a ~/cf-ddns/cf-ddns.log
+        echo "$PUBLIC_IP = Public IP" | tee -a ~/cf-ddns/cf-ddns.log
+else
+        curl --request PUT \
+          --url https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID \
+          --header 'Content-Type: application/json' \
+          --header "Authorization: Bearer $TOKEN" \
+          --data $data_json
+fi
